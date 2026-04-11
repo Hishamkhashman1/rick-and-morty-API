54c6bbb74015052a089771631ae1a2702973e235
Hisham <hisham.khashman@gmail.com>
2026-04-12 01:24:27 +0900

    two paths configured with decorators

--- app/main.py
+++ app/main.py
@@ -11,26 +11,6 @@ async def get_characters(character_id: int):
         if character["id"] == character_id:
             return character
 
-
-from fastapi import FastAPI
-from fastapi import FastAPI
-import json
-app = FastAPI()
-
-with open("/home/hisham/code/Hishamkhashman1/rick-and-morty-API/data/characters.json", "r") as f:
-    data = json.load(f)
-
-@app.get("/characters/{character_id}")
-async def get_characters(character_id: int):
-    return {character_id: character_id}
-
-
-
 @app.get("/characters")
 async def get_characters():
     return data
-
-
-
-
-
