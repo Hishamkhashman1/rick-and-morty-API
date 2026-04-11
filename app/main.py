b0cc4d731f23fe722f7902ee04f66dbc3b792cf6
Hisham <hisham.khashman@gmail.com>
2026-04-11 23:28:34 +0900

    made simple MVP functional API

--- app/main.py
+++ app/main.py
@@ -5,9 +5,31 @@ app = FastAPI()
 with open("/home/hisham/code/Hishamkhashman1/rick-and-morty-API/data/characters.json", "r") as f:
     data = json.load(f)
 
-@app.get("/characters/character_id")
-async def get_characters():
+@app.get("/characters/{character_id}")
+async def get_characters(character_id: int):
+    for character in data:
+        if character["id"] == character_id:
+            return character
+
+
+from fastapi import FastAPI
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+with open("/home/hisham/code/Hishamkhashman1/rick-and-morty-API/data/characters.json", "r") as f:
+    data = json.load(f)
+
+@app.get("/characters/{character_id}")
+async def get_characters(character_id: int):
     return {character_id: character_id}
+
+
+
+@app.get("/characters")
+async def get_characters():
+    return data
+
 
 
 
