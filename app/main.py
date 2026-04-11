55393185d03d37cdaf4b37ce1fa2077ff723ca42
Hisham <hisham.khashman@gmail.com>
2026-04-11 22:33:59 +0900

    added import json

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+with open("/home/hisham/code/Hishamkhashman1/rick-and-morty-API/data/characters.json", "r") as f:
+    data = json.load(f)
 
 @app.get("/characters/character_id")
 async def get_characters():
